b30870b100d51635171be996739b4b6861207e60
Boyang Matsapola <boyangbass@gmail.com>
2019-09-21 16:21:07 -0500

    changed db

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "K@giso72",
-  database: "burgers_db"
+  user: "rm63jdru53877l5b",
+  password: "z2waq94mrdag6lds",
+  database: "ekvr5mb47ynfcq2s"
 });
 
 // Make connection.
